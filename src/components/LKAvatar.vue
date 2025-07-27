4a51f9c204b5378606713515a7559373b1670650
ly <276366000@qq.com>
2025-07-27 15:41:19 +0800

    7.24am

--- src/components/LKAvatar.vue
+++ src/components/LKAvatar.vue
@@ -1,5 +1,5 @@
 <template>
-  <n-avatar color="" :size="size" :class="props.alpha ? [`opacity-${props.alpha}`] : []">
+  <n-avatar color="#" :size="size" :class="props.alpha ? [`opacity-${props.alpha}`] : []">
     <LkIcon :size="48" />
   </n-avatar>
 </template>
